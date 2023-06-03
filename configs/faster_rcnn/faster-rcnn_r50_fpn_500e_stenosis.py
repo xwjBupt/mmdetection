ae3274ef8d5b568ec1ff3d8523d626d580681d2c
xwjBupt <xwj_bupt@163.com>
2023-06-03 17:07:51 +0800

    faster-rcnn_r50_fpn_500e_stenosis/500epoch-baserun

--- configs/faster_rcnn/faster-rcnn_r50_fpn_500e_stenosis.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_500e_stenosis.py
@@ -4,16 +4,28 @@ _base_ = [
     "../_base_/schedules/schedule_2x.py",
     "../_base_/default_runtime.py",
 ]
-model = dict(
-    _delete_=True,
-    data_preprocessor=dict(
-        type="DetDataPreprocessor",
-        mean=[144.5754766729963, 144.5754766729963, 144.5754766729963],
-        std=[55.8710224233549, 55.8710224233549, 55.8710224233549],
-        bgr_to_rgb=True,
-        pad_size_divisor=32,
-    ),
+
+data_preprocessor = dict(
+    type="DetDataPreprocessor",
+    mean=[144.5754766729963, 144.5754766729963, 144.5754766729963],
+    std=[55.8710224233549, 55.8710224233549, 55.8710224233549],
+    bgr_to_rgb=True,
+    pad_size_divisor=32,
 )
+
+
+# data_preprocessor = dict(
+#     type="DetDataPreprocessor",
+#     mean=[123.675, 116.28, 103.53],
+#     std=[58.395, 57.12, 57.375],
+#     bgr_to_rgb=True,
+#     pad_size_divisor=1,
+#     pad_mask=True,
+#     mask_pad_value=0,
+#     pad_seg=True,
+#     seg_pad_value=255,
+# )
+
 vis_backends = [
     dict(
         type="WandbVisBackend",
