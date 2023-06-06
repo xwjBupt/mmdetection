3b8c47e5def393f055f66cbcf79cc05b0f9c81c2
xwjBupt <xwj_bupt@163.com>
2023-06-06 15:27:35 +0800

    retinanet_swin-t-p4-w7_fpn_500e_stenosis/500epoch-newbaserun

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,9 +4,7 @@ default_hooks = dict(
     timer=dict(type="IterTimerHook"),
     logger=dict(type="LoggerHook", interval=50),
     param_scheduler=dict(type="ParamSchedulerHook"),
-    checkpoint=dict(
-        type="CheckpointHook", interval=1, save_best="auto", max_keep_ckpts=5
-    ),
+    checkpoint=dict(type="CheckpointHook", interval=1, save_best="auto"),
     sampler_seed=dict(type="DistSamplerSeedHook"),
     visualization=dict(type="DetVisualizationHook"),
 )
--- configs/_base_/schedules/schedule_500e.py
+++ configs/_base_/schedules/schedule_500e.py
@@ -20,6 +20,7 @@ optim_wrapper = dict(
         type="SGD", lr=base_lr, momentum=0.9, weight_decay=5e-4, nesterov=True
     ),
     paramwise_cfg=dict(norm_decay_mult=0.0, bias_decay_mult=0.0),
+    clip_grad=dict(max_norm=35, norm_type=2),
 )
 
 # learning rate
