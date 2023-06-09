6eee8532893ae2d31d0eba0aafb3fb0857fe1a94
xwjBupt <xwj_bupt@163.com>
2023-06-09 16:31:29 +0800

    yolox_l_8xb8-1000e-lr5e-2_stenosis_binary_F1/fold1

---
+++ configs/yolox/yolox_l_8xb8-1000e-lr5e-2_stenosis_binary_F1.py
@@ -0,0 +1,37 @@
+_base_ = "./yolox_l_8xb8-1000e-lr5e-2_stenosis_binary.py"
+
+
+dataset_type = "CocoStenosisBinaryDataset"
+data_root = "/ai/mnt/data/stenosis/selected/Binary/FOLD1/"
+dataset_name = "STENOSIS_BINARY"
+train_ann_file = "annotations/train_binary.json"
+val_ann_file = "annotations/val_binary.json"
+train_data_prefix = dict(img="train/")
+val_data_prefix = dict(img="val/")
+
+train_dataset = dict(
+    dataset=dict(
+        type="CocoStenosisBinaryDataset",
+        data_root="/ai/mnt/data/stenosis/selected/Binary/FOLD1/",
+        ann_file="annotations/train_binary.json",
+        data_prefix=dict(img="train/"),
+    )
+)
+
+train_dataloader = dict(batch_size=8, num_workers=4, dataset=train_dataset)
+val_dataloader = dict(
+    batch_size=16,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        data_root="/ai/mnt/data/stenosis/selected/Binary/FOLD1/",
+        ann_file="annotations/val_binary.json",
+        data_prefix=dict(img="val/"),
+    ),
+)
+test_dataloader = val_dataloader
+val_evaluator = dict(
+    ann_file="/ai/mnt/data/stenosis/selected/Binary/FOLD1/"
+    + "annotations/val_binary.json"
+)
+test_evaluator = val_evaluator
